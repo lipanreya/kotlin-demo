95e54244df716a80bf3741b1be1cd7895baacffc
lipanre <lipanre@qq.com>
2024-09-21 10:55:19 +0800

    feat: 项目初始提交

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,13 @@
+/**
+ * {@code description}
+ *
+ *
+ * <br>
+ * {@code date} 2024/9/21 10:53
+ */
+
+fun main(args: Array<String>) {
+    println("What's your name?")
+    val name = readln()
+    println("Hello, $name!")
+}
